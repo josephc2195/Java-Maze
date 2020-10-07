9dbf75a809c207c98c47837fde8977798130d41d
Joseph <chicaj1221@gmail.com>
2020-10-07 00:31:02 -0400

    first commit

---
+++ maze.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class Maze {
+
+  public static void main(String args[]) {
+    new Maze();
+  }
+
+  public void Maze() {
+    super("Maze");
+  }
+
+  public void init() {
+
+  }
+
+
+}
