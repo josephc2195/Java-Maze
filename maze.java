46982d336402229997cbea84e423e72236a0a33b
Joseph <chicaj1221@gmail.com>
2020-10-12 19:05:26 -0400

    update

--- maze.java
+++ maze.java
@@ -9,15 +9,25 @@ public class Maze {
   }
 
   public void Maze() {
-    super("Maze");
+
   }
 
   public void init() {
-
+    char wall_char = '#';
+    char start_char = 's';
+    char end_char = 'e';
+    char open_char = ' ';
   }
 
-  private void buildNew(int width = 0, int height = 0) {
-
+  private void buildNew(int width, int height) {
+    for(int i = 0; i < height; i++) {
+      for(int x = 0; x < width; x++) {
+        Point p = (i, x);
+        if(p.x == 0 || p.x == 1 || p.y == 0 || p.y == 1) {
+          
+        }
+      }
+    }
   }
 
 
