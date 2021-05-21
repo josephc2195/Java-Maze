856edefa08ab31c81dfef395f81a7469d7a481b7
josephc2195 <chicaj1221@gmail.com>
2021-05-20 21:27:35 -0400

    Asking user for size

--- Game.java
+++ Game.java
@@ -1,17 +1,25 @@
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.*;
+// import javax.swing.JButton;
+// import javax.swing.JFrame;
+// import javax.swing.JMenu;
+// import javax.swing.JMenuBar;
+// import javax.swing.JMenuItem;
+// import javax.swing.JPanel;
+// import javax.swing.JTextField;
+// import javax.swing.SwingUtilities;
 
 public class Game extends JFrame{
         int width = 10;
         int height = 10;
         int cellSize = 25;
         Maze mz = new Maze(width, height);
+        String input;
 
         public Game() {
-                mazeUI();
+                inputUI();
+                //mazeUI();
+
         }
 
         public static void main(String[] args) {
@@ -23,11 +31,30 @@ public class Game extends JFrame{
                 });
         }
 
+        private void inputUI() {
+                JPanel text = new JPanel();
+                JTextField userSize = new JTextField(10);
+                JButton submit = new JButton("Submit");
+                JLabel ask = new JLabel("Please enter the size you want the maze to be.");
+                setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+                text.add(userSize);
+                add(ask);
+                add(text);
+                add(submit);
+                setSize(400, 150);
+                setTitle("Please enter a Size");
+                input = userSize.getText();     
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+                setLocationRelativeTo(null);          
+        }
+
         private void mazeUI() {
                 JMenuBar mb = new JMenuBar();
                 JMenu file = new JMenu("File");
+                JMenuItem nw = new JMenuItem("New");
+                file.add(nw);
                 mb.add(file);
-                setSize(width * cellSize + 225, height * cellSize + 50);
+                setSize(width * cellSize + 225, height * cellSize + 100);
                 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 setTitle("Chicas Apps: Maze Game");
                 DisplayMaze dm = new DisplayMaze(mz, cellSize);
@@ -39,5 +66,5 @@ public class Game extends JFrame{
                 setLocationRelativeTo(null);
         }
         
-        //public void installMenuItem(JMenu menu, JMenuItem item, )
+        
 }
